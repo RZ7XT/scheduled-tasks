deb99cb0452ebeb7343845c8d51f0d1b996521c4
RZ7XT <144817509+RZ7XT@users.noreply.github.com>
2026-04-19 13:25:05 +0300

    Integrate flight search and refactor weather alerts
    Refactor weather alert logic and integrate flight search functionality.

--- main.py
+++ main.py
@@ -1,72 +1,30 @@
-import requests
-import os
-from twilio.rest import Client
+from data_manager import DataManager
+from pprint import pprint
+from datetime import datetime, timedelta
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager
 
-OWN_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-my_api = os.environ.get("OWN_API")
-account_sid = os.environ.get("ACC_SID")
-auth_token = os.environ.get("AUTH_TKN")
+notification_manager = NotificationManager()
 
-client = Client(account_sid, auth_token)
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
 
-# 32.040917
-# 35.931787
-params = {
-    "lat": 32.040917,
-    "lon": 35.931787,
-    "appid": my_api,
-    "cnt": 4,
-}
+today = datetime.today().date()
+tomorrow = today + timedelta(days=1)
+six_months_from_today = today + timedelta(hours=4392)
 
-response = requests.get(url=OWN_Endpoint, params=params)
-response.raise_for_status()
-weather_data = response.json()
+flight_search = FlightSearch()
+for destination in sheet_data:
+    pprint(f"Getting flights for {destination['city']}...")
+    my_flight_data = flight_search.check_flights(origin_city_code="AMM",
+                                destination_city_code=destination['iataCode'],
+                                from_time=tomorrow,
+                                to_time=six_months_from_today)
+    cheapest_flight = find_cheapest_flight(my_flight_data, return_date=six_months_from_today)
+    pprint(f"{destination['city']}: {cheapest_flight.price} JOD")
 
-
-id_list = []
-
-thunderstorm = False
-rain = False
-light_rain = False
-fog = False
-snow = False
-
-for three_hour_window in range(3):
-    id_num = weather_data["list"][three_hour_window]["weather"][0]["id"]
-    id_list.append(id_num)
-
-for weatherper3 in id_list:
-    if 600 <= weatherper3 <= 622:
-        snow = True
-    elif 200 <= weatherper3 <= 232:
-        thunderstorm = True
-    elif 500 <= weatherper3 <= 531:
-        rain = True
-    elif 300 <= weatherper3 <= 321:
-        light_rain = True
-    elif 701 <= weatherper3 <= 781:
-        fog = True
-
-# Send one message based on priority
-if thunderstorm:
-    body = "⛈ Thunderstorm Alert ⛈\n\nThunderstorms expected today. Bring a strong jacket and stay indoors if possible.\nAvoid open areas and be careful of lightning. ⚡"
-elif snow:
-    body = "❄️ Snow Alert ❄️\n\nSnow expected. Dress warmly and wear boots.\nWatch for slippery surfaces and keep warm. 🧤🧣"
-elif rain:
-    body = "🌧 Rain Alert 🌧\n\nRain expected. Bring a waterproof jacket and umbrella.\nTravel carefully and avoid puddles. ☔"
-elif light_rain:
-    body = "🌦 Light Rain Alert 🌦\n\nDrizzle expected. Carry a light jacket or umbrella.\nFootwear with grip is recommended to avoid slipping. ☂️"
-elif fog:
-    body = "🌫 Visibility Alert 🌫\n\nLow visibility expected due to fog, haze, or dust.\nDrive carefully and watch your surroundings. 👀"
-else:
-    body = "☀️ Weather Update ☀️\n\nNo rain expected. Wear your usual clothes and enjoy the day! 😎"
-
-message = client.messages.create(
-    from_="+13185944642",
-    body=body,
-    to="+962795466752"
-)
-print(message.status)
-
-
-
+    if cheapest_flight.price != "N/A" and int(cheapest_flight.price) < int(destination["lowestPrice"]):
+        pprint(f"Lower price flight found to {destination['city']}!")
+        data_manager.update_lowest_price(destination["id"], cheapest_flight.price)
+        notification_manager.send_telegram_message(f"Low price alert for {destination['city']}!\n\n Only {cheapest_flight.price} JOD to travel from AMM to {destination["iataCode"]}, on {tomorrow.strftime("%Y/%m/%d")} --> {six_months_from_today.strftime("%Y/%m/%d")}")
