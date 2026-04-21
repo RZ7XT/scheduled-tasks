8a05e156fa2f3ed66573e465d2270055dd30af7f
RZ7XT <144817509+RZ7XT@users.noreply.github.com>
2026-04-21 07:55:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ sheet_data = data_manager.get_destination_data()
 
 today = datetime.today().date()
 tomorrow = today + timedelta(days=1)
-six_months_from_today = today + timedelta(hours=4392)
+one_week_from_today = today + timedelta(hours=132)
 
 flight_search = FlightSearch()
 for destination in sheet_data:
@@ -20,10 +20,10 @@ for destination in sheet_data:
                                 destination_city_code=destination['iataCode'],
                                 from_time=tomorrow,
                                 to_time=six_months_from_today)
-    cheapest_flight = find_cheapest_flight(my_flight_data, return_date=six_months_from_today)
+    cheapest_flight = find_cheapest_flight(my_flight_data, return_date=one_week_from_today)
     print(f"{destination['city']}: {cheapest_flight.price} JOD")
 
     if cheapest_flight.price != "N/A" and int(cheapest_flight.price) < int(destination["lowestPrice"]):
         print(f"Lower price flight found to {destination['city']}!")
         data_manager.update_lowest_price(destination["id"], cheapest_flight.price)
-        notification_manager.send_telegram_message(f"Low price alert for {destination['city']}!\n\n Only {cheapest_flight.price} JOD to travel from AMM to {destination["iataCode"]}, on {tomorrow.strftime("%Y/%m/%d")} --> {six_months_from_today.strftime("%Y/%m/%d")}")
+        notification_manager.send_telegram_message(f"Low price alert for {destination['city']}!\n\n Only {cheapest_flight.price} JOD to travel from AMM to {destination["iataCode"]}, on {tomorrow.strftime("%Y/%m/%d")} --> {one_week_from_today.strftime("%Y/%m/%d")}")
