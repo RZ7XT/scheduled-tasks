c7c42b676db267eb671c89e575fae80a28f70c3f
RZ7XT <144817509+RZ7XT@users.noreply.github.com>
2026-04-23 16:43:02 +0300

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -6,7 +6,7 @@ class DataManager:
     def __init__(self):
 
         self.SHEETY_BEARER = os.environ.get("FD_SHEETY_BEARER")
-        self.sheety_url = "https://api.sheety.co/ad3d56f727b1cc13a3c5387c56d7f9e5/copyOfFlightDeals/prices"
+        self.sheety_url = "https://api.sheety.co/a48f21f851b75862d9bc81bc6d5cfc39/qusai'sFlightTwin/prices"
         self.authorization = {
             "Authorization": f"Bearer {self.SHEETY_BEARER}"
         }
